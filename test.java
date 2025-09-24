fea25ca6f219305d70b80a38e996418310331195
Ucheawaji Anne Dienye <dienyeucheawaji93@gmail.com>
2025-09-24 14:37:20 +0100

    test

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public class test
    //----------------------------------------------------
    //  Prints a statement.
    //----------------------------------------------------
-   public static void man (String[] args)
+   public static void main (String[] args)
    {
       System.out.bogus ("An emergency Broadcast");
    }
